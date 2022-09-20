27727f36bc17b8708c94a3146f3fbb8fa1ebb749
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2022-09-19 21:51:58 -0500

    cambios

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ function saludo(){
 function despedir(){
     alert("Adios desde el salon de clase :0");
 }
+function botonP(){
+    alert("Este boton de escribio en casa :0");
+}
 
-console.log("hola desde la consola :3 ");
+console.log("hola desde la consola de mi casa :3 ");
 console.log("Este es un texto");
-console.log("20");
+console.log("250");
 console.log("2.6");
 console.log("1,63");
 console.log("1+2");
