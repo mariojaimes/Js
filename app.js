621489c7c77f8c7f8312b1ceb1a1cf2d393eb0d8
mariojaimes <54184058+mariojaimes@users.noreply.github.com>
2022-09-19 21:40:16 -0500

    add

---
+++ app.js
@@ -0,0 +1,15 @@
+function saludo(){
+    alert("Hola desde el salon de clase :0");
+}
+function despedir(){
+    alert("Adios desde el salon de clase :0");
+}
+
+console.log("hola desde la consola :3 ");
+console.log("Este es un texto");
+console.log("20");
+console.log("2.6");
+console.log("1,63");
+console.log("1+2");
+console.log("true");
+console.log("false");
